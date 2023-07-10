1b322bd38b706138ab8e3e65195cff4b4486b61e
BradyFoster <Bjfoster9860@gmail.com>
2023-07-10 16:33:24 -0700

    Fetched data from db.json

--- index.js
+++ index.js
@@ -1,93 +1,30 @@
+const bossUrl = "http://localhost:3000/data"
 
-const search = document.getElementById("search")
-const results = document.getElementById("results")
-const clearButton = document.getElementById("clearButton")
-const recentlyViewed = document.getElementById("recentlyViewed")
-const loading = document.getElementById("loading")
-const errorMessage = document.getElementById("errorMessage")
+let bossArray = []
 
-const maxRecentlyViewed = 5
-let recentlyViewedItems = []
-
-search.addEventListener("submit",function(event) {
-    event.preventDefault()
-    
-    const name = event.target.name.value
-    const updatedName = name.replace("_").toLowerCase()
-
-    loading.style.display = "block"
-    results.innerHTML = " "
-    errorMessage.textContent = " "
-
-    
-    fetch("http://localhost:3000/data")
-    .then(function(response) {
-        if(response.status !== 200) {
-            console.log("Error occured. The boss you entered was not found on https://eldenring.fanapis.com/api/bosses")
-        }
-        return response.json()
+const getCharacters = (url) => {
+    fetch(url)
+    .then(response => response.json()) 
+    .then(characters => {
+        characters.forEach(character => bossArray.push(character))
+        characterList(bossArray)
     })
-    .then(function(data) {
-        let items = data
+}
 
-        items.map(function(item) {
-            let itemName = document.createElement("h2")
-            let itemImage = document.createElement("h2")
-            let itemDescription = document.createElement("h2")
+let bossAttributes = {name: [], region: [], description: [], location: []}
 
-             itemName.textContent = `${item.name}`
-             itemImage.src = `${item.image}`
-             itemDescription.textContent = `${item.description}`
-
-
-             results.appendChild(itemName)
-             results.appendChild(itemImage)
-             results.appendChild(itemDescription)
-
-             addToRecentlyViewed(updatedName)
-             updateRecentlyViewed()
-        })
-            
-            })
-             
-            
-    })
-    
-
-    clearButton.addEventListener('click', function() {
-        search.reset();
-        results.innerHTML = '';
-        errorMessage.textContent = '';
-        recentlyViewed.innerHTML = '';
-        recentlyViewedItems = [];
-      });
-
-
-      recentlyViewed.addEventListener('click', function(event) {
-        if (event.target.classList.contains('recentlyViewedItem')) {
-          const clickedItem = event.target.dataset.thing;
-          if (clickedItem) {
-            const nameInput = document.getElementById('name');
-            nameInput.value = clickedItem;
-          }
+const createObjectOfCharacterValues = () => {
+    for(let index in bossArray) {
+        let characterInfo = bossArray[index]
+        for (let charKey in characterInfo) {
+            if(charKey !== "id" && charKey !== "name" && charKey !== "image") {
+                let attValue = bossAttributes[charKey]
+                let objAtKey = characterInfo[charKey]
+                let infoOption = attValue.find(element => element === objAtKey)
+                if (infoOption === undefined) {
+                    attValue.push(objAtKey)
+                }
+            }
         }
-      });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }
+}
