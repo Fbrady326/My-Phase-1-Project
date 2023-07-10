d570089302ae988c3b3061cebd1ec321faef9206
BradyFoster <Bjfoster9860@gmail.com>
2023-07-10 02:58:50 -0700

    Added event listener and fecthed data

--- index.js
+++ index.js
@@ -1,3 +1,76 @@
+
 const search = document.getElementById("search")
 const results = document.getElementById("results")
 const clearButton = document.getElementById("clearButton")
+const recentlyViewed = document.getElementById("recentlyViewed")
+const loading = document.getElementById("loading")
+const errorMessage = document.getElementById("errorMessage")
+
+const maxRecentlyViewed = 5
+let recentlyViewedItems = []
+
+search.addEventListener("submit",function(event) {
+    event.preventDefault()
+    
+    const name = event.target.name.value
+    const updatedName = name.replace("_").toLowerCase()
+
+    loading.style.display = "block"
+    results.innerHTML = " "
+    errorMessage.textContent = " "
+
+    
+    fetch("http://localhost:3000/data")
+    .then(function(response) {
+        if(response.status !== 200) {
+            console.log("Error occured. The boss you entered was not found on https://eldenring.fanapis.com/api/bosses")
+        }
+        return response.json()
+    })
+    .then(function(data) {
+        let items = data
+
+        items.map(function(item) {
+            let itemName = document.createElement("h2")
+            let itemImage = document.createElement("h2")
+            let itemDescription = document.createElement("h2")
+
+             itemName.textContent = `${item.name}`
+             itemImage.src = `${item.image}`
+             itemDescription.textContent = `${item.description}`
+
+
+             results.appendChild(itemName)
+             results.appendChild(itemImage)
+             results.appendChild(itemDescription)
+
+             addToRecentlyViewed(updatedName)
+             updateRecentlyViewed()
+        })
+            
+            })
+             
+            
+    })
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
