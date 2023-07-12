e399f4336ddd61446c4e7bec831f359782019c3d
BradyFoster <Bjfoster9860@gmail.com>
2023-07-12 13:23:56 -0700

    Added more to index.js and style.css

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const bossDataTemplate = document.querySelector("[bossDataTemplate]")
+const bossDataTemplate= document.querySelector("[bossDataTemplate]")
+const bossDataCards= document.querySelector("[bossDataCards]")
 
 fetch("https://eldenring.fanapis.com/api/bosses")
 .then(function(response) {
@@ -8,6 +9,17 @@ fetch("https://eldenring.fanapis.com/api/bosses")
     //let items = data.data
     data.data.forEach(item => {
     const card = bossDataTemplate.content.cloneNode(true).children[0]
+    const header = card.querySelector("[dataHeader]") 
+    const body = card.querySelector("[dataBody]")
+    const region = card.querySelector("[dataRegion]")
+    const location = card.querySelector("[dataLocation]")
+    const image = card.querySelector("[dataImage]")
+    header.textContent = item.name
+    body.textContent = item.description 
+    region.textContent = item.region
+    location.textContent = item.location
+    image.src = item.image
+    bossDataCards.append(card)
     console.log(item)
     })
 })
